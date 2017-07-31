6a45a58a23452c8d202a1f969d412d09795c1802
Your Name <you@example.com>
2017-07-31 17:08:05 +0300

    home_task

---
+++ javascript/PaulLapkin.js
@@ -0,0 +1,43 @@
+$(document).ready(function(e) {
+        
+
+        //alert('Привет')
+
+        
+
+        $('.button-hire-me').click(function() {
+
+        	$('.button-hire-me').addClass('button-hire-me-bg');
+        });
+
+
+        $('.menu_close').click(
+        	function(){
+        		$('.header_aside').hide(300);
+        	}
+        	);
+        $('.button-menu').click(
+        	function(){
+        		$('.header_aside').show(300);
+        }
+        );
+        $('.list_menu').click(
+        	function(){
+        		$('.header_aside').hide(300);
+
+        	}
+        	);
+        $('.button-hire-me').mouseenter(
+        	function(){
+        		$('.tooltip').show(300);
+
+        	}
+        	);
+        $('.button-hire-me').mouseout(
+        	function(){
+        		$('.tooltip').hide(300)
+        	}
+        	);
+
+
+});
